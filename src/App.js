68d2a8bdbc06341cfe6ac9bb5fbb89da233b5c7a
Bala S <balas@Balas-MacBook-Pro.local>
2022-05-28 01:40:01 +0530

    Main Commit

---
+++ src/App.js
@@ -0,0 +1,142 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { ConnectButton, Modal } from "web3uikit";
+import logo from "./images/Moralis.png";
+import Coin from "./components/Coin"
+import { modalGlobalConfig } from "antd/lib/modal/confirm";
+import { abouts } from "./about";
+import { useMoralisWeb3Api, useMoralis } from "react-moralis";
+//import color from "web3uikit/dist/styles/colors";
+
+const App = () => {
+
+
+  const [btc, setBtc] = useState(50);
+  const [eth, setEth] = useState(50);
+  const [ada, setAda] = useState(50);
+  const [matic, setMatic] = useState(50);
+  const [modalPrice, setModalPrice] = useState();
+  const Web3Api = useMoralisWeb3Api();
+  const {Moralis,isInitialized} = useMoralis();
+  const [visible, setVisible] = useState(false);
+  const [modalToken, setModalToken] = useState();
+
+  async function getRatio(tick,setPerc){
+
+    const votes = Moralis.Object.extend("Votes");
+    const query = new Moralis.Query(votes);
+    query.equalTo("ticker",tick);
+    query.descending("createdAt");
+    const results = await query.first();
+    let up = Number(results.attributes.up);
+    let down = Number(results.attributes.down);
+    let ratio = Math.round(up/(up+down)*100);
+    setPerc(ratio);
+  }
+
+  useEffect(()=>{
+    if(isInitialized){
+      getRatio("BTC",setBtc);
+      getRatio("ETH",setEth);
+      getRatio("ADA",setAda);
+      getRatio("MATIC",setMatic);
+    }
+
+    async function createLiveQuery(){
+      let query = new Moralis.Query('Votes');
+      let subs = await query.subscribe();
+      subs.on('update',(object) => {
+        getRatio("BTC",setBtc);
+        getRatio("ETH",setEth);
+        getRatio("ADA",setAda);
+        getRatio("MATIC",setMatic);
+        if(object.attributes.ticker=="BTC") getRatio("BTC",setBtc);
+        else if (object.attributes.ticker=="ETH") getRatio("ETH",setEth);
+        else if (object.attributes.ticker=="ADA") getRatio("ADA",setAda);
+        else if (object.attributes.ticker=="MATIC") getRatio("MATIC",setMatic);       
+      });
+    }
+
+    createLiveQuery();
+  })
+  useEffect(() => {
+    async function fetchTokenPrice(){
+      const options = {
+        address: 
+          abouts[abouts.findIndex((x) => x.token === modalToken)].address,
+      };
+      const price = await Web3Api.token.getTokenPrice(options);
+      setModalPrice(price.usdPrice.toFixed(4));
+    }
+
+    if(modalToken){ fetchTokenPrice() }
+  }, [modalToken]);
+  
+  return (
+    <>
+      <div className="header">
+        <div className="logo">
+          <img src={logo} alt="logo" height="50px" />
+          Market Sentiment
+        </div>
+        <ConnectButton />
+      </div>
+
+      <div className="instructions">
+        Analyse.Predict.Vote <br></br>
+        Pick a Trend for your favourite coin
+      </div>
+      <div className="list">
+        <Coin
+          perc={btc}
+          setPerc={setBtc}
+          token={"BTC"}
+          setModalToken={setModalToken}
+          setVisible={setVisible} />
+        <Coin
+          perc={eth}
+          setPerc={setEth}
+          token={"ETH"}
+          setModalToken={setModalToken}
+          setVisible={setVisible} />
+        <Coin
+          perc={ada}
+          setPerc={setAda}
+          token={"ADA"}
+          setModalToken={setModalToken}
+          setVisible={setVisible} />
+        <Coin
+          perc={matic}
+          setPerc={setMatic}
+          token={"MATIC"}
+          setModalToken={setModalToken}
+          setVisible={setVisible} />
+      </div>
+
+      <Modal
+        isVisible={visible}
+        onCloseButtonPressed={() => setVisible(false)}
+        hasFooter={false}
+        title={modalToken}>
+          <div>
+            <span style={{color: "silver"}}>{`Price: `}</span>
+            {modalPrice}$
+          </div>
+
+          <div>
+            <span style={{color: "gold"}}>{`About`}</span>
+          </div>
+          <div>
+            {modalToken && 
+            abouts[abouts.findIndex((x) => x.token === modalToken)].about}
+          </div>
+
+      </Modal>
+
+    </>
+
+
+  );
+};
+
+export default App;
---
+++ src/components/Coin.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import "./Coin.css";
+import { Button } from "web3uikit";
+import { useWeb3ExecuteFunction, useMoralis } from "react-moralis";
+
+
+function Coin({ perc, setPerc, token, setModalToken, setVisible }) {
+
+
+  const [color, setColor] = useState();
+  const contract = useWeb3ExecuteFunction();
+  const { isAuthenticated } = useMoralis();
+
+  useEffect(() => {
+    if (perc < 50) {
+      setColor("red");
+    } else {
+      setColor("green");
+    }
+  }, [perc]);
+
+  async function vote(updown) {
+    let options = {
+      contractAddress: "0x85eFFfb71e97F97f268F8C15efcEcF3E8A00D3A6",
+      functionName: "addVote",
+      abi: [{
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "tickerName",
+            "type": "string"
+          },
+          {
+            "internalType": "bool",
+            "name": "vote",
+            "type": "bool"
+          }
+        ],
+        "name": "addVote",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }],
+      params: {
+        tickerName: token,
+        vote: updown
+      },
+    }
+
+    await contract.fetch({
+      params: options,
+      onSuccess: () => { console.log("Voting Successfull"); },
+      onError: (error) => { alert(error.data.message); }
+    });
+  }
+  return (
+    <>
+      <div>
+        <div className="token">
+          {token}
+        </div>
+        <div className="circle" style={{ boxShadow: `0 0 20px ${color}` }}>
+          <div className="wave"
+            style={{
+              marginTop: `${100 - perc}%`,
+              boxShadow: `0 0 20px ${color}`,
+              backgroundColor: "black",
+            }}>
+
+          </div>
+          <div className="percentage">
+            {perc}%
+          </div>
+        </div>
+        <div className="votes">
+          <Button
+            onClick={() => {
+              if (isAuthenticated) { vote(true) }
+              else { alert("Authenticate to vote!!") }
+              
+            }}
+            text="•UP•"
+            theme="primary"
+            type="button"
+          />
+          <Button
+            color="red"
+            onClick={() => {
+              if (isAuthenticated) { vote(false) }
+              else { alert("Authenticate to vote!!") }
+
+            }}
+            text="•DOWN•"
+            theme="colored"
+            type="button"
+          />
+
+        </div>
+        <div className="votes">
+          <Button
+            onClick={() => {
+              setModalToken(token)
+              setVisible(true);
+            }}
+            text="•INFO•"
+            theme="translucent"
+            type="button"
+          />
+
+        </div>
+
+
+
+      </div>
+    </>
+  );
+}
+
+export default Coin;
